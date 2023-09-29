9fb715f9182e80f389c54caad47d50b698e4aeba
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-29 20:17:31 +0800

    Update functions.php

--- kleandrive-child/functions.php
+++ kleandrive-child/functions.php
@@ -5,7 +5,7 @@
 
 // Load rtl.css because it is not autoloaded from the child theme
 if ( ! function_exists( 'planty_child_load_rtl' ) ) {
-	add_filter( 'wp_enqueue_scripts', 'planty_child_load_rtl', 99999 );
+	add_filter( 'wp_enqueue_scripts', 'planty_child_load_rtl');
 	function planty_child_load_rtl() {
 		if ( is_rtl() ) {
 			wp_enqueue_style( 'planty-style-rtl', get_template_directory_uri() . '/rtl.css' );
