1fc8bf567477e1db77f2739d0743b26ed9443def
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-18 17:50:14 +0800

    Update archive-careers.php

--- kleandrive-child/archive-careers.php
+++ kleandrive-child/archive-careers.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying any archive page.
  */
@@ -17,11 +18,11 @@ $terms = _get_terms_details('careers-category');
 				</div>
 				<div class="inner">
 					<select id="location" name="location" class="nice-select-js nice-select-style-1 nice-select-transparent">
-						<option value="" > All Locations </option>
-						<?php if($terms) { ?>
-							<?php foreach($terms as $key => $term) { ?>
-								<?php 
-								if($main_query->term_id == $key)  {
+						<option value=""> All Locations </option>
+						<?php if ($terms) { ?>
+							<?php foreach ($terms as $key => $term) { ?>
+								<?php
+								if ($main_query->term_id == $key) {
 									$selected = 'selected';
 								} else {
 									$selected = '';
@@ -35,7 +36,61 @@ $terms = _get_terms_details('careers-category');
 			</div>
 			<div id="results">
 				<div class="results-holder">
+					<div class="row">
+						<?php
+						if (have_posts()) {
+							while (have_posts()) {
+								the_post();
+								$company_logo = carbon_get_the_post_meta('company_logo');
+								if (get_post_type() == 'inthepress') {
+									$permalink = carbon_get_the_post_meta('redirect_url');
+									$target = 'target="_blank"';
+								} else {
+									$permalink = get_the_permalink();
+									$target = '';
+								}
+						?>
+								<div class="col-lg-4 col-md-6 post-item">
+									<div class="column-holder post-holder">
+										<article class="post post-<?= get_the_ID()  ?>">
+											<a href="<?= $permalink ?>" class="post-image-link" <?= $target ?>>
+												<?php
+												$DisplayData->image(get_post_thumbnail_id(), 'large', false);
+												?>
+												<?php if ($post_type == 'post') { ?>
+													<div class="date">
+														<?= get_the_date('jS F Y') ?>
+													</div>
+												<?php } ?>
+											</a>
+											<div class="content-holder <?= $company_logo ? 'has-logo' : '' ?>">
+												<?php get_template_part('template-parts/archive/archive', 'category') ?>
 
+												<?php if ($company_logo) { ?>
+													<?php $DisplayData->image(carbon_get_the_post_meta('company_logo'), 'medium', false, 'company-logo') ?>
+												<?php } ?>
+
+												<a href="<?= $permalink ?>" <?= $target ?>>
+													<?php
+													$DisplayData->heading(get_the_title(), 'h3', false, false)
+													?>
+												</a>
+											</div>
+
+											<a href="<?= $permalink ?>" class="arrow-con background-accent arrow-box" <?= $target ?>>
+												<span class="arrow right"><span></span></span>
+											</a>
+										</article>
+									</div>
+								</div>
+							<?php }
+						} else {
+							?>
+							<h2>No Results Found</h2>
+						<?php
+						}
+						?>
+					</div>
 				</div>
 			</div>
 			<div class="load-more text-center">
@@ -55,7 +110,7 @@ $terms = _get_terms_details('careers-category');
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body contact-form-v2">
-					<?= do_shortcode( carbon_get_theme_option('careers_contact_form') ) ?>
+					<?= do_shortcode(carbon_get_theme_option('careers_contact_form')) ?>
 				</div>
 			</div>
 		</div>
@@ -68,7 +123,7 @@ $terms = _get_terms_details('careers-category');
 	jQuery(document).ready(function($) {
 		jQuery('#location').change();
 
-		jQuery(document).on("click", '.apply-button', function(event) { 
+		jQuery(document).on("click", '.apply-button', function(event) {
 			$title = jQuery(this).attr('data-title');
 			jQuery('input[name="position"]').val($title);
 			jQuery('.modal-title span').text($title);
@@ -84,10 +139,10 @@ $terms = _get_terms_details('careers-category');
 		});
 	});
 
-	jQuery('#applyModal').on('show.bs.modal', function (e) {
+	jQuery('#applyModal').on('show.bs.modal', function(e) {
 		jQuery('html').addClass('overflow-hidden');
 	})
-	jQuery('#applyModal').on('hide.bs.modal', function (e) {
+	jQuery('#applyModal').on('hide.bs.modal', function(e) {
 		jQuery('html').removeClass('overflow-hidden');
 	})
 </script>
