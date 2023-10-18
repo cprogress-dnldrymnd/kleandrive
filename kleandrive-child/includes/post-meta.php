6554eabfd38a41274cb4c2ee98c6f41b9b833305
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-18 18:13:48 +0800

    Update post-meta.php

--- kleandrive-child/includes/post-meta.php
+++ kleandrive-child/includes/post-meta.php
@@ -7,15 +7,6 @@ use Carbon_Fields\Field;
 /*-----------------------------------------------------------------------------------*/
 /* Career Settings
 /*-----------------------------------------------------------------------------------*/
-Container::make('theme_options', __('Careers Settings'))
-    ->set_page_parent('edit.php?post_type=careers')
-    ->add_tab('General Settings', array(
-        Field::make('text', 'careers_alt_title', 'Alt Title'),
-        Field::make('textarea', 'careers_description', 'Description'),
-    ))
-    ->add_tab('Contact Form', array(
-        Field::make('text',   'careers_contact_form_shortcode', 'Contact Form Shortcode'),
-    ));
 
 
 Container::make('post_meta', __('Careers Details'))
