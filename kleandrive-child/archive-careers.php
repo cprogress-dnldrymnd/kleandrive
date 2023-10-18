6ed472ffd7fac622553e9e11511358c993965345
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-18 17:51:01 +0800

    Update archive-careers.php

--- kleandrive-child/archive-careers.php
+++ kleandrive-child/archive-careers.php
@@ -36,60 +36,86 @@ $terms = _get_terms_details('careers-category');
 			</div>
 			<div id="results">
 				<div class="results-holder">
-					<div class="row">
+					<div class="career-wrapper">
 						<?php
-						if (have_posts()) {
-							while (have_posts()) {
-								the_post();
-								$company_logo = carbon_get_the_post_meta('company_logo');
-								if (get_post_type() == 'inthepress') {
-									$permalink = carbon_get_the_post_meta('redirect_url');
-									$target = 'target="_blank"';
-								} else {
-									$permalink = get_the_permalink();
-									$target = '';
-								}
-						?>
-								<div class="col-lg-4 col-md-6 post-item">
-									<div class="column-holder post-holder">
-										<article class="post post-<?= get_the_ID()  ?>">
-											<a href="<?= $permalink ?>" class="post-image-link" <?= $target ?>>
-												<?php
-												$DisplayData->image(get_post_thumbnail_id(), 'large', false);
-												?>
-												<?php if ($post_type == 'post') { ?>
-													<div class="date">
-														<?= get_the_date('jS F Y') ?>
+						while (have_posts()) {
+							the_post(); ?>
+							<?php
+							$postterms = get_the_terms(get_the_ID(), 'location');
+							$salary = carbon_get_the_post_meta('salary');
+							$accordion = carbon_get_the_post_meta('accordion');
+							?>
+
+							<div class="career-holder background-white post-item">
+								<div class="inner">
+									<div class="header justify-space-between">
+										<div class="career-title align-center">
+											<h3><?php the_title() ?></h3>
+											<span class="salary">£ <?= $salary ?></span>
+										</div>
+										<div class="location align-center">
+											<svg xmlns="http://www.w3.org/2000/svg" width="10.908" height="15.583" viewBox="0 0 10.908 15.583">
+												<path id="Icon_material-location-on" data-name="Icon material-location-on" d="M12.954,3A5.45,5.45,0,0,0,7.5,8.454c0,4.091,5.454,10.129,5.454,10.129s5.454-6.038,5.454-10.129A5.45,5.45,0,0,0,12.954,3Zm0,7.4A1.948,1.948,0,1,1,14.9,8.454,1.949,1.949,0,0,1,12.954,10.4Z" transform="translate(-7.5 -3)" fill="#001f2b" />
+											</svg>
+											<span>
+												<?php foreach ($postterms as $postterm) { ?>
+													<span><?= $postterm->name ?></span>
+												<?php } ?>
+											</span>
+										</div>
+									</div>
+									<div class="body">
+										<div class="career-description d-none d-sm-block">
+											<?php the_content() ?>
+										</div>
+										<?php if ($accordion) { ?>
+											<div class="accordion-holder accordion-style-1">
+												<div class="accordion" id="accordion-<?= get_the_ID() ?>">
+													<div class="accordion-item d-block d-sm-none">
+														<h2 class="accordion-header" id="heading<?= get_the_ID() . '-description'  ?>">
+															<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= get_the_ID() . '-description'  ?>" aria-expanded="false" aria-controls="collapse<?= get_the_ID() . '-description'  ?>">
+																<i class="fa-solid fa-plus"></i>
+																<i class="fa-solid fa-minus"></i>
+																<span> Job Description </span>
+															</button>
+														</h2>
+														<div id="collapse<?= get_the_ID() . '-description'  ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= get_the_ID() . '-description'  ?>" data-bs-parent="#accordion-<?= get_the_ID() ?>">
+															<div class="accordion-body">
+																<?php the_content() ?>
+															</div>
+														</div>
 													</div>
-												<?php } ?>
-											</a>
-											<div class="content-holder <?= $company_logo ? 'has-logo' : '' ?>">
-												<?php get_template_part('template-parts/archive/archive', 'category') ?>
+													<?php foreach ($accordion as $key => $acc) { ?>
+														<div class="accordion-item">
+															<h2 class="accordion-header" id="heading<?= get_the_ID() . '-' . $key ?>">
+																<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= get_the_ID() . '-' . $key ?>" aria-expanded="false" aria-controls="collapse<?= get_the_ID() . '-' . $key ?>">
+																	<i class="fa-solid fa-plus"></i>
+																	<i class="fa-solid fa-minus"></i>
+																	<span> <?= $acc['accordion_title'] ?></span>
+																</button>
+															</h2>
+															<div id="collapse<?= get_the_ID() . '-' . $key ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= get_the_ID() . '-' . $key ?>" data-bs-parent="#accordion-<?= get_the_ID() ?>">
+																<div class="accordion-body">
+																	<?= wpautop($acc['accordion_content']) ?>
+																</div>
+															</div>
+														</div>
+													<?php } ?>
 
-												<?php if ($company_logo) { ?>
-													<?php $DisplayData->image(carbon_get_the_post_meta('company_logo'), 'medium', false, 'company-logo') ?>
-												<?php } ?>
-
-												<a href="<?= $permalink ?>" <?= $target ?>>
-													<?php
-													$DisplayData->heading(get_the_title(), 'h3', false, false)
-													?>
-												</a>
+												</div>
 											</div>
-
-											<a href="<?= $permalink ?>" class="arrow-con background-accent arrow-box" <?= $target ?>>
-												<span class="arrow right"><span></span></span>
-											</a>
-										</article>
+										<?php } ?>
+									</div>
+									<div class="footer">
+										<div class="button-box text-right">
+											<button type="button" class="button accent-button apply-button" data-title="<?php the_title() ?>" data-bs-toggle="modal" data-bs-target="#applyModal">
+												<span>APPLY FOR THIS POSITION</span><span class="arrow right white rotated"><span></span></span>
+											</button>
+										</div>
 									</div>
 								</div>
-							<?php }
-						} else {
-							?>
-							<h2>No Results Found</h2>
-						<?php
-						}
-						?>
+							</div>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
