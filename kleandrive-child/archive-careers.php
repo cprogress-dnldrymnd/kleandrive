7c316cc12f14495b9f295c0b6d7ae83b5520a411
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-18 18:10:53 +0800

    Update archive-careers.php

--- kleandrive-child/archive-careers.php
+++ kleandrive-child/archive-careers.php
@@ -52,19 +52,9 @@ $terms = _get_terms_details('careers-category');
 							<div class="career-holder background-white post-item">
 								<div class="inner">
 									<div class="header justify-space-between">
-										<div class="career-title align-center">
+										<div class="career-title align-center d-flex align-items-center justify-content-between">
 											<h3><?php the_title() ?></h3>
 											<span class="salary">£ <?= $salary ?></span>
-										</div>
-										<div class="location align-center">
-											<svg xmlns="http://www.w3.org/2000/svg" width="10.908" height="15.583" viewBox="0 0 10.908 15.583">
-												<path id="Icon_material-location-on" data-name="Icon material-location-on" d="M12.954,3A5.45,5.45,0,0,0,7.5,8.454c0,4.091,5.454,10.129,5.454,10.129s5.454-6.038,5.454-10.129A5.45,5.45,0,0,0,12.954,3Zm0,7.4A1.948,1.948,0,1,1,14.9,8.454,1.949,1.949,0,0,1,12.954,10.4Z" transform="translate(-7.5 -3)" fill="#001f2b" />
-											</svg>
-											<span>
-												<?php foreach ($postterms as $postterm) { ?>
-													<span><?= $postterm->name ?></span>
-												<?php } ?>
-											</span>
 										</div>
 									</div>
 									<div class="body">
