e60fd461b3430bbd52b8cc8b87f68f36c7c6d608
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-19 20:59:56 +0800

    Update footer-cta.php

--- kleandrive-child/template-parts/footer-cta.php
+++ kleandrive-child/template-parts/footer-cta.php
@@ -3,6 +3,10 @@ $overwrite_footer_cta = carbon_get_the_post_meta('overwrite_footer_cta');
 $heading = carbon_get_the_post_meta('heading');
 $button_text = carbon_get_the_post_meta('button_text');
 $button_url = carbon_get_the_post_meta('button_url');
+
+$footer_cta_heading = carbon_get_theme_option('footer_cta_heading');
+$footer_cta_button_text = carbon_get_theme_option('footer_cta_button_text');
+$footer_cta_button_url = carbon_get_theme_option('footer_cta_button_url');
 ?>
 
 <section class="footer-cta">
@@ -15,7 +19,7 @@ $button_url = carbon_get_the_post_meta('button_url');
                         if ($overwrite_footer_cta && $heading) {
                             echo $heading;
                         } else {
-                            echo 'Find out how we can help you';
+                            echo $footer_cta_heading;
                         }
                         ?>
                         
