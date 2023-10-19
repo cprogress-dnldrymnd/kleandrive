59be459aaee1e30e8aa0733a52aeee445f82f7a7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-19 20:53:48 +0800

    Update footer-cta.php

--- kleandrive-child/template-parts/footer-cta.php
+++ kleandrive-child/template-parts/footer-cta.php
@@ -1,14 +1,24 @@
-<?php 
-
+<?php
+$overwrite_footer_cta = carbon_get_the_post_meta('overwrite_footer_cta');
+$heading = carbon_get_the_post_meta('heading');
+$button_text = carbon_get_the_post_meta('button_text');
+$button_url = carbon_get_the_post_meta('button_url');
 ?>
 
 <section class="footer-cta">
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                <?php
-
-                ?>
+                <div class="elementor-element elementor-element-ec63353 footer-contact sc_fly_static elementor-widget elementor-widget-trx_sc_title trx_addons_parallax_layers_inited trx_addons_parallax_blocks_inited" data-element_type="widget" data-widget_type="trx_sc_title.default">
+                    <div class="elementor-widget-container">
+                        <div class="sc_title sc_title_default">
+                            <h2 class="sc_item_title sc_title_title sc_item_title_style_default">
+                                <span class="sc_item_title_text">Find out how we can help you
+                                </span>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
