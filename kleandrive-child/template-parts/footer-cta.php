8a6b7adbeb4718e347e173c8c8cca339c6b14797
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-19 20:47:47 +0800

    cc

---
+++ kleandrive-child/template-parts/footer-cta.php
@@ -0,0 +1,11 @@
+<div class="elementor-element e-flex e-con-boxed e-con e-parent" data-element_type="container">
+    <div class="e-con-inner">
+        <div class="elementor-element elementor-element-7239087 footer-contact sc_fly_static elementor-widget elementor-widget-trx_sc_title trx_addons_parallax_layers_inited trx_addons_parallax_blocks_inited" data-id="7239087" data-element_type="widget" data-widget_type="trx_sc_title.default">
+            <div class="elementor-widget-container">
+                <div class="sc_title sc_title_default">
+                    <h2 class="sc_item_title sc_title_title sc_item_title_style_default"><span class="sc_item_title_text">[footer_cta_heading]</span></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
