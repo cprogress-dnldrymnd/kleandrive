a31c43d7f89a19a3b638e834ea9f81ccdd670ac1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-20 20:02:19 +0800

    Update page-template-process.php

--- kleandrive-child/templates/page-template-process.php
+++ kleandrive-child/templates/page-template-process.php
@@ -9,7 +9,28 @@
 
 <?php if ($process) { ?>
     <section class="our-process-page">
+        <div class="container">
+            <?php foreach ($process as $proc) { ?>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="column-holder">
 
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="column-holder">
+                            <div class="stage"><?= $proc['stage'] ?></div>
+                            <div class="heading-box">
+                                <h2><?= $proc['heading'] ?></h2>
+                            </div>
+                            <div class="description-box">
+                                <?= wpautop($proc['description']) ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </section>
 <?php } ?>
 
