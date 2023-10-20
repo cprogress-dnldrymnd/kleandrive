68c7e7d778fcf4fe18e06992f1e45f9274a41cc2
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-20 21:09:26 +0800

    Update page-template-process.php

--- kleandrive-child/templates/page-template-process.php
+++ kleandrive-child/templates/page-template-process.php
@@ -9,6 +9,9 @@
 <section class="page-heading">
     <div class="container">
         <h1><?php the_title() ?></h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+        </svg>
     </div>
 </section>
 <?php if ($process) { ?>
