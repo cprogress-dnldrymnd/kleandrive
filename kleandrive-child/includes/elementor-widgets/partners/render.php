99bc76a90f07aaabdffaa01fb4859b4c39ccb0c1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 21:29:53 +0800

    cc

--- kleandrive-child/includes/elementor-widgets/partners/render.php
+++ kleandrive-child/includes/elementor-widgets/partners/render.php
@@ -8,42 +8,44 @@ $args = array(
 $partners = get_posts($args);
 ?>
 <div class="partner-slider <?= $style ?>">
-    <div class="swiper mySwiperPartner">
-        <div class="swiper-wrapper">
-            <?php foreach ($partners as $partner) { ?>
-                <?php
-                $logo = carbon_get_post_meta($partner->ID, 'logo');
-                $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
-                $website = carbon_get_post_meta($partner->ID, 'website');
-                ?>
-                <div class="swiper-slide">
-                    <div class="inner">
-                        <div class="description-box">
-                            <?= $partner->post_content ?>
-                        </div>
+    <div class="partner-wrapper">
+        <div class="swiper mySwiperPartner">
+            <div class="swiper-wrapper">
+                <?php foreach ($partners as $partner) { ?>
+                    <?php
+                    $logo = carbon_get_post_meta($partner->ID, 'logo');
+                    $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
+                    $website = carbon_get_post_meta($partner->ID, 'website');
+                    ?>
+                    <div class="swiper-slide">
+                        <div class="inner">
+                            <div class="description-box">
+                                <?= $partner->post_content ?>
+                            </div>
 
-                        <?php if ($style == 'partner-style-1') { ?>
-                            <div class="logo-box">
-                                <?php if ($logo) { ?>
-                                    <div class="image">
-                                        <img src="<?= wp_get_attachment_image_url($logo, 'medium') ?>" alt="<?= $partner->post_title ?>">
-                                    </div>
-                                <?php } ?>
-
-                                <div class="title-holder">
-                                    <div class="title"><?= $partner->post_title ?></div>
-                                    <?php if ($website) { ?>
-                                        <a href="<?= $website ?>" class="website"><?= $website ?></a>
+                            <?php if ($style == 'partner-style-1') { ?>
+                                <div class="logo-box">
+                                    <?php if ($logo) { ?>
+                                        <div class="image">
+                                            <img src="<?= wp_get_attachment_image_url($logo, 'medium') ?>" alt="<?= $partner->post_title ?>">
+                                        </div>
                                     <?php } ?>
 
+                                    <div class="title-holder">
+                                        <div class="title"><?= $partner->post_title ?></div>
+                                        <?php if ($website) { ?>
+                                            <a href="<?= $website ?>" class="website"><?= $website ?></a>
+                                        <?php } ?>
+
+                                    </div>
                                 </div>
-                            </div>
-                        <?php } ?>
+                            <?php } ?>
+                        </div>
                     </div>
-                </div>
-            <?php }  ?>
+                <?php }  ?>
+            </div>
+            <div class="swiper-pagination swiper-pagination-style"></div>
         </div>
-        <div class="swiper-pagination swiper-pagination-style"></div>
     </div>
 </div>
 <?
