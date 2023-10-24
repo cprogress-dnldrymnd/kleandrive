e4313b1b3ffa112db29fcd777a17c84ac5c0cb59
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 19:52:26 +0800

    Update partners.php

--- kleandrive-child/includes/elementor-widgets/partners.php
+++ kleandrive-child/includes/elementor-widgets/partners.php
@@ -32,5 +32,48 @@ if (get_current_user_id() == 1) {
         {
             return ['partners'];
         }
+
+        protected function register_controls()
+        {
+
+            $this->start_controls_section(
+                'content_section',
+                [
+                    'label' => esc_html__('Content', 'elementor-oembed-widget'),
+                    'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+                ]
+            );
+
+            $this->add_control(
+                'url',
+                [
+                    'label' => esc_html__('URL to embed', 'elementor-oembed-widget'),
+                    'type' => \Elementor\Controls_Manager::TEXT,
+                    'input_type' => 'url',
+                    'placeholder' => esc_html__('https://your-link.com', 'elementor-oembed-widget'),
+                ]
+            );
+
+            $this->end_controls_section();
+        }
+
+        /**
+         * Render oEmbed widget output on the frontend.
+         *
+         * Written in PHP and used to generate the final HTML.
+         *
+         * @since 1.0.0
+         * @access protected
+         */
+        protected function render()
+        {
+
+            $settings = $this->get_settings_for_display();
+            $html = wp_oembed_get($settings['url']);
+
+            echo '<div class="oembed-elementor-widget">';
+            echo ($html) ? $html : $settings['url'];
+            echo '</div>';
+        }
     }
 }
