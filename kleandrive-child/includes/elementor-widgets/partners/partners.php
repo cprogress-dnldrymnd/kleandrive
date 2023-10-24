4dfc8aecd4dcb61d71df37c2a8fe7de19defb043
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 20:13:05 +0800

    cc

--- kleandrive-child/includes/elementor-widgets/partners/partners.php
+++ kleandrive-child/includes/elementor-widgets/partners/partners.php
@@ -1,90 +1,88 @@
 <?php
-if (get_current_user_id() == 1) {
-    class Elementor_Partners extends \Elementor\Widget_Base
+class Elementor_Partners extends \Elementor\Widget_Base
+{
+
+    public function get_name()
+    {
+        return 'widget_name';
+    }
+
+    public function get_title()
+    {
+        return esc_html__('Partners', 'textdomain');
+    }
+
+    public function get_icon()
+    {
+        return 'eicon-code';
+    }
+
+    public function get_custom_help_url()
+    {
+        return 'https://go.elementor.com/widget-name';
+    }
+
+    public function get_categories()
+    {
+        return ['basic'];
+    }
+
+    public function get_keywords()
+    {
+        return ['partners'];
+    }
+
+    protected function register_controls()
     {
 
-        public function get_name()
-        {
-            return 'widget_name';
-        }
-
-        public function get_title()
-        {
-            return esc_html__('Partners', 'textdomain');
-        }
-
-        public function get_icon()
-        {
-            return 'eicon-code';
-        }
-
-        public function get_custom_help_url()
-        {
-            return 'https://go.elementor.com/widget-name';
-        }
-
-        public function get_categories()
-        {
-            return ['basic'];
-        }
-
-        public function get_keywords()
-        {
-            return ['partners'];
-        }
-
-        protected function register_controls()
-        {
-
-            $this->start_controls_section(
-                'content_section',
-                [
-                    'label' => esc_html__('Partners Slider', 'elementor-oembed-widget'),
-                    'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-                ]
-            );
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__('Partners Slider', 'elementor-oembed-widget'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
 
 
-            $this->add_control(
-                'important_note',
-                [
-                    'label' => esc_html__('', 'textdomain'),
-                    'type' => \Elementor\Controls_Manager::RAW_HTML,
-                    'raw' => esc_html__('This widget will display partners slider', 'textdomain'),
-                    'content_classes' => '',
-                ]
-            );
+        $this->add_control(
+            'important_note',
+            [
+                'label' => esc_html__('', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::RAW_HTML,
+                'raw' => esc_html__('This widget will display partners slider', 'textdomain'),
+                'content_classes' => '',
+            ]
+        );
 
-            $this->add_control(
-                'style',
-                [
-                    'label' => esc_html__( 'Style', 'textdomain' ),
-                    'type' => \Elementor\Controls_Manager::SELECT,
-                    'default' => 'partner-style-1',
-                    'options' => [
-                        'partner-style-1' => esc_html__( 'Style 1', 'textdomain' ),
-                        'partner-style-2' => esc_html__( 'Style 2', 'textdomain' ),
-                    ],
-                ]
-            );
+        $this->add_control(
+            'style',
+            [
+                'label' => esc_html__('Style', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => 'partner-style-1',
+                'options' => [
+                    'partner-style-1' => esc_html__('Style 1', 'textdomain'),
+                    'partner-style-2' => esc_html__('Style 2', 'textdomain'),
+                ],
+            ]
+        );
 
-            $this->end_controls_section();
-        }
+        $this->end_controls_section();
+    }
 
-        /**
-         * Render oEmbed widget output on the frontend.
-         *
-         * Written in PHP and used to generate the final HTML.
-         *
-         * @since 1.0.0
-         * @access protected
-         */
-        protected function render()
-        {
+    /**
+     * Render oEmbed widget output on the frontend.
+     *
+     * Written in PHP and used to generate the final HTML.
+     *
+     * @since 1.0.0
+     * @access protected
+     */
+    protected function render()
+    {
 
-            $settings = $this->get_settings_for_display();
+        $settings = $this->get_settings_for_display();
 
-            include(__DIR__ . '/render.php');
-        }
+        include(__DIR__ . '/render.php');
     }
 }
--- kleandrive-child/includes/elementor.php
+++ kleandrive-child/includes/elementor.php
@@ -1,12 +1,10 @@
 <?php
-if (get_current_user_id() == 1) {
 
-    function register_new_widgets($widgets_manager)
-    {
+function register_new_widgets($widgets_manager)
+{
 
-        require_once( __DIR__  . '/elementor-widgets/partners/partners.php');
+    require_once(__DIR__  . '/elementor-widgets/partners/partners.php');
 
-        $widgets_manager->register(new \Elementor_Partners());
-    }
-    add_action('elementor/widgets/register', 'register_new_widgets');
+    $widgets_manager->register(new \Elementor_Partners());
 }
+add_action('elementor/widgets/register', 'register_new_widgets');
