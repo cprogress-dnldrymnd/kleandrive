a275ae583c6f4a95eb037e3cfe8406c45cf7cdff
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 19:49:22 +0800

    cc

--- kleandrive-child/elementor-widgets/partners.php
+++ kleandrive-child/elementor-widgets/partners.php
@@ -1,1 +1,36 @@
-dsdsds
+<?php
+if (get_current_user_id() == 1) {
+    class Elementor_Partners extends \Elementor\Widget_Base
+    {
+
+        public function get_name()
+        {
+            return 'widget_name';
+        }
+
+        public function get_title()
+        {
+            return esc_html__('My Widget Name', 'textdomain');
+        }
+
+        public function get_icon()
+        {
+            return 'eicon-code';
+        }
+
+        public function get_custom_help_url()
+        {
+            return 'https://go.elementor.com/widget-name';
+        }
+
+        public function get_categories()
+        {
+            return ['kleandrive'];
+        }
+
+        public function get_keywords()
+        {
+            return ['partners'];
+        }
+    }
+}
---
+++ kleandrive-child/includes/elementor.php
@@ -0,0 +1,9 @@
+<?php
+function register_new_widgets( $widgets_manager ) {
+
+	require_once( get_stylesheet_directory_uri(). '/elementor-widgets/partners.php' );
+
+	$widgets_manager->register( new \Elementor_Partners() );
+
+}
+add_action( 'elementor/widgets/register', 'register_new_widgets' );
