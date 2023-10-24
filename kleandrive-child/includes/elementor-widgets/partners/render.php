90275d4c7dce3c1494400eefc100e0953a21f485
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 21:53:07 +0800

    Update render.php

--- kleandrive-child/includes/elementor-widgets/partners/render.php
+++ kleandrive-child/includes/elementor-widgets/partners/render.php
@@ -9,27 +9,7 @@ $partners = get_posts($args);
 ?>
 <div class="partner-slider <?= $style ?>">
     <div class="partner-wrapper">
-        <?php if ($style == 'partner-style-2') { ?>
-            <div thumbsSlider="" class="swiper mySwiperPartnerThumbImages">
-                <div class="swiper-wrapper">
-                    <?php foreach ($partners as $partner) { ?>
-                        <?php
-                        $logo = carbon_get_post_meta($partner->ID, 'logo');
-                        $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
 
-                        $logo_val = $alt_logo ? $alt_logo : $logo;
-                        ?>
-                        <div class="swiper-slide">
-                            <?php if ($logo) { ?>
-                                <div class="image-box">
-                                    <img src="<?= wp_get_attachment_image_url($logo_val, 'medium') ?>" alt="<?= $partner->post_title ?>">
-                                </div>
-                            <?php } ?>
-                        </div>
-                    <?php } ?>
-                </div>
-            </div>
-        <?php } ?>
 
         <div class="swiper mySwiperPartner <?= $style == 'partner-style-2' ? 'mySwiperPartnerThumb' : 'mySwiperPartnernoThumb' ?>">
             <div class="swiper-wrapper">
@@ -75,6 +55,27 @@ $partners = get_posts($args);
                 <?php }  ?>
             </div>
         </div>
+        <?php if ($style == 'partner-style-2') { ?>
+            <div thumbsSlider="" class="swiper mySwiperPartnerThumbImages">
+                <div class="swiper-wrapper">
+                    <?php foreach ($partners as $partner) { ?>
+                        <?php
+                        $logo = carbon_get_post_meta($partner->ID, 'logo');
+                        $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
+
+                        $logo_val = $alt_logo ? $alt_logo : $logo;
+                        ?>
+                        <div class="swiper-slide">
+                            <?php if ($logo) { ?>
+                                <div class="image-box">
+                                    <img src="<?= wp_get_attachment_image_url($logo_val, 'medium') ?>" alt="<?= $partner->post_title ?>">
+                                </div>
+                            <?php } ?>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        <?php } ?>
     </div>
     <div class="swiper-pagination swiper-pagination-style"></div>
 </div>
