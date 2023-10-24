9fba365e2bbc22798c8293692c5a99a04116e040
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 20:45:25 +0800

    cc

--- kleandrive-child/includes/elementor-widgets/partners/partners-script.js
+++ kleandrive-child/includes/elementor-widgets/partners/partners-script.js
@@ -1,11 +1,13 @@
-var mySwiperText = new Swiper(".mySwiperPartner", {
-    loop: true,
-    autoplay: true,
-    slidesPerView: 1,
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-        clickable: true
-    },
+jQuery(document).ready(function () {
+    const mySwiperPartner = new Swiper(".mySwiperPartner", {
+        loop: true,
+        autoplay: true,
+        slidesPerView: 1,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+            clickable: true
+        },
+    });
+    
 });
-
