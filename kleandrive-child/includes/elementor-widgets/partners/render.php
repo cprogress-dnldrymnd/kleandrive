623dc8c779d203aead5e20b749cacc3c69e717d8
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-24 20:17:24 +0800

    Update render.php

--- kleandrive-child/includes/elementor-widgets/partners/render.php
+++ kleandrive-child/includes/elementor-widgets/partners/render.php
@@ -1,6 +1,23 @@
 <?php
 $style = $settings['style'];
-echo $style;
-?>
+$args = array(
+    'numberposts' => -1,
+    'post_type'   => 'partners'
+);
 
+$partners = get_posts($args);
+?>
+<div class="partner-slider <?= $style ?>">
+    <div class="swiper-wrapper">
+        <?php foreach ($partners as $partner) { ?>
+            <div class="swiper-slide">
+                <div class="inner">
+                    <div class="description-box">
+                        <?= $partner->post_content ?>
+                    </div>
+                </div>
+            </div>
+        <?php }  ?>
+    </div>
+</div>
 <?
