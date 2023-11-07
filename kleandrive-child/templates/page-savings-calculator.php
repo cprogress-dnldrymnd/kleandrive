ab9c3ed08f37a4ae3d433097c1b949423d61ff1b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-07 19:38:00 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -32,39 +32,42 @@ $description = carbon_get_the_post_meta('description');
             <h4 class="mb-4">
                 Kleanbus model assumptions
             </h4>
-            <form id="calculator">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                            <label for="annual_mileage">Annual mileage (km)</label>
-                            <input type="text" class="form-control" id="annual_mileage" name="annual_mileage">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                            <label for="remaining_life">Remaining life (Yrs)</label>
-                            <input type="text" class="form-control" id="remaining_life" name="remaining_life">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                            <label for="no_of_buses_converted">No of buses converted</label>
-                            <input type="text" class="form-control" id="no_of_buses_converted" name="no_of_buses_converted">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
+            <div id="calculator">
 
-                        <div class="form-group">
-                            <label for="est_annual_op_cost">Estimated annual operational costs for existing bus</label>
-                            <input type="text" class="form-control" id="est_annual_op_cost" name="est_annual_op_cost">
+                <form>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="annual_mileage">Annual mileage (km)</label>
+                                <input type="text" class="form-control" id="annual_mileage" name="annual_mileage">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="remaining_life">Remaining life (Yrs)</label>
+                                <input type="text" class="form-control" id="remaining_life" name="remaining_life">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="no_of_buses_converted">No of buses converted</label>
+                                <input type="text" class="form-control" id="no_of_buses_converted" name="no_of_buses_converted">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+
+                            <div class="form-group">
+                                <label for="est_annual_op_cost">Estimated annual operational costs for existing bus</label>
+                                <input type="text" class="form-control" id="est_annual_op_cost" name="est_annual_op_cost">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <button type="submit" class="btn btn-secondary">Submit</button>
                         </div>
                     </div>
-
-                    <div class="col-lg-12">
-                        <button type="submit" class="btn btn-secondary">Submit</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 </section>
