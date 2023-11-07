d469ee8ef2be783b26cf9112c8c7f50a716dcb37
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-07 19:36:36 +0800

    css

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -33,25 +33,38 @@ $description = carbon_get_the_post_meta('description');
                 Kleanbus model assumptions
             </h4>
             <form id="calculator">
-                <div class="form-group">
-                    <label for="annual_mileage">Annual mileage (km)</label>
-                    <input type="text" class="form-control" id="annual_mileage" name="annual_mileage">
-                </div>
-                <div class="form-group">
-                    <label for="remaining_life">Remaining life (Yrs)</label>
-                    <input type="text" class="form-control" id="remaining_life" name="remaining_life">
-                </div>
+                <div class="row">
 
-                <div class="form-group">
-                    <label for="no_of_buses_converted">No of buses converted</label>
-                    <input type="text" class="form-control" id="no_of_buses_converted" name="no_of_buses_converted">
-                </div>
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <label for="annual_mileage">Annual mileage (km)</label>
+                            <input type="text" class="form-control" id="annual_mileage" name="annual_mileage">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <label for="remaining_life">Remaining life (Yrs)</label>
+                            <input type="text" class="form-control" id="remaining_life" name="remaining_life">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <label for="no_of_buses_converted">No of buses converted</label>
+                            <input type="text" class="form-control" id="no_of_buses_converted" name="no_of_buses_converted">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
 
-                <div class="form-group">
-                    <label for="est_annual_op_cost">Estimated annual operational costs for existing bus</label>
-                    <input type="text" class="form-control" id="est_annual_op_cost" name="est_annual_op_cost">
+                        <div class="form-group">
+                            <label for="est_annual_op_cost">Estimated annual operational costs for existing bus</label>
+                            <input type="text" class="form-control" id="est_annual_op_cost" name="est_annual_op_cost">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12">
+                        <button type="submit" class="btn btn-secondary">Submit</button>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
