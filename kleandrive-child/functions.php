de90fe566fe2f40677ec4455306514faf0a323e4
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-08 18:40:34 +0800

    css

--- kleandrive-child/functions.php
+++ kleandrive-child/functions.php
@@ -171,3 +171,13 @@ function action_wp_footer()
 }
 
 add_action('wp_footer', 'action_wp_footer');
+
+
+add_filter('body_class', 'custom_class');
+function custom_class($classes)
+{
+	if (is_category()) {
+		$classes[] = 'body_style_fullwide';
+	}
+	return $classes;
+}
