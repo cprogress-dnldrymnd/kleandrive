f98f02ef961124bc6197bfe96ad7d6d04b516a3e
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-23 22:28:36 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -331,6 +331,17 @@ $description = carbon_get_the_post_meta('description');
                 'min': [0],
                 'max': [100000]
             },
+            format: {
+                // 'to' the formatted value. Receives a number.
+                to: function(value) {
+                    return value + ',-';
+                },
+                // 'from' the formatted value.
+                // Receives a string, should return a number.
+                from: function(value) {
+                    return Number(value.replace(',-', ''));
+                }
+            }
         });
     });
 </script>
