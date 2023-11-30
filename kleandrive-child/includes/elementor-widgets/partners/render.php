500cb0c0a3fdea40e2e170fdded1aa040739d883
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 16:14:34 +0800

    Update render.php

--- kleandrive-child/includes/elementor-widgets/partners/render.php
+++ kleandrive-child/includes/elementor-widgets/partners/render.php
@@ -19,65 +19,97 @@ if ($group) {
 $partners = get_posts($args);
 ?>
 <div class="partner-slider <?= $style ?>">
-    <div class="partner-wrapper">
-        <div class="swiper mySwiperPartner mySwiperPartnernoThumb">
-            <div class="swiper-wrapper">
+    <?php if ($style != 'partner-style-3') { ?>
+        <div class="partner-wrapper">
+            <div class="swiper mySwiperPartner mySwiperPartnernoThumb">
+                <div class="swiper-wrapper">
+                    <?php foreach ($partners as $partner) { ?>
+                        <?php
+                        $logo = get_post_thumbnail_id($partner->ID);
+                        $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
+                        $website = carbon_get_post_meta($partner->ID, 'website');
+                        $logo_val = $alt_logo ? $alt_logo : $logo;
+                        ?>
+                        <div class="swiper-slide">
+                            <div class="inner">
+                                <div class="description-box">
+                                    <?= $partner->post_content ?>
+                                </div>
+
+                                <?php if ($style == 'partner-style-1') { ?>
+                                    <div class="logo-box">
+                                        <?php if ($logo) { ?>
+                                            <div class="image">
+                                                <img src="<?= wp_get_attachment_image_url($logo, 'medium') ?>" alt="<?= $partner->post_title ?>">
+                                            </div>
+                                        <?php } ?>
+
+                                        <div class="title-holder">
+                                            <div class="title"><?= $partner->post_title ?></div>
+                                            <?php if ($website) { ?>
+                                                <a target="_blank" href="<?= $website ?>" class="website"><?= $website ?></a>
+                                            <?php } ?>
+
+                                        </div>
+                                    </div>
+                                <?php } else { ?>
+
+                                    <?php if ($logo_val) { ?>
+                                        <div class="image-box">
+                                            <img src="<?= wp_get_attachment_image_url($logo_val, 'medium') ?>" alt="<?= $partner->post_title ?>">
+                                        </div>
+                                    <?php } ?>
+                                    <?php $title = get_post_meta($partner->ID, 'trx_addons_options', true)['subtitle'] ?>
+                                    <div class="name-box">
+                                        <div class="name"><?= $partner->post_title ?></div>
+                                        <div class="title"><?= $title ?></div>
+                                    </div>
+
+                                    <?php if ($website) { ?>
+                                        <div class="sc_item_button sc_button_wrap">
+                                            <a target="_blank" href="<?= $website ?>" class="apply-button sc_button sc_button_bordered sc_button_size_normal sc_button_icon_left color_style_link3">
+                                                <span class="sc_button_text"><span class="sc_button_title">Visit Website</span></span>
+                                            </a>
+                                        </div>
+                                    <?php } ?>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination swiper-pagination-style"></div>
+    <?php } else { ?>
+        <div class="partner-wrapper">
+            <div class="row">
                 <?php foreach ($partners as $partner) { ?>
                     <?php
                     $logo = get_post_thumbnail_id($partner->ID);
                     $alt_logo = carbon_get_post_meta($partner->ID, 'alt_logo');
                     $website = carbon_get_post_meta($partner->ID, 'website');
+                    $partner_description = carbon_get_post_meta($partner->ID, 'partner_description');
                     $logo_val = $alt_logo ? $alt_logo : $logo;
                     ?>
-                    <div class="swiper-slide">
-                        <div class="inner">
-                            <div class="description-box">
-                                <?= $partner->post_content ?>
-                            </div>
-
-                            <?php if ($style == 'partner-style-1') { ?>
-                                <div class="logo-box">
-                                    <?php if ($logo) { ?>
-                                        <div class="image">
-                                            <img src="<?= wp_get_attachment_image_url($logo, 'medium') ?>" alt="<?= $partner->post_title ?>">
-                                        </div>
-                                    <?php } ?>
-
-                                    <div class="title-holder">
-                                        <div class="title"><?= $partner->post_title ?></div>
-                                        <?php if ($website) { ?>
-                                            <a target="_blank" href="<?= $website ?>" class="website"><?= $website ?></a>
-                                        <?php } ?>
-
-                                    </div>
+                    <div class="col-lg-4">
+                        <div class="column-holder">
+                            <?php if ($logo_val) { ?>
+                                <div class="image-box">
+                                    <img src="<?= wp_get_attachment_image_url($logo_val, 'medium') ?>" alt="<?= $partner->post_title ?>">
                                 </div>
-                            <?php } else { ?>
-
-                                <?php if ($logo) { ?>
-                                    <div class="image-box">
-                                        <img src="<?= wp_get_attachment_image_url($logo_val, 'medium') ?>" alt="<?= $partner->post_title ?>">
-                                    </div>
-                                <?php } ?>
-                                <?php $title = get_post_meta($partner->ID, 'trx_addons_options', true)['subtitle'] ?>
-                                <div class="name-box">
-                                    <div class="name"><?= $partner->post_title ?></div>
-                                    <div class="title"><?= $title ?></div>
-                                </div>
-
-                                <?php if ($website) { ?>
-                                    <div class="sc_item_button sc_button_wrap">
-                                        <a target="_blank" href="<?= $website ?>" class="apply-button sc_button sc_button_bordered sc_button_size_normal sc_button_icon_left color_style_link3">
-                                            <span class="sc_button_text"><span class="sc_button_title">Visit Website</span></span>
-                                        </a>
-                                    </div>
-                                <?php } ?>
                             <?php } ?>
+                            <div class="heading-box">
+                                <h2><?= $partner->post_title ?></h2>
+                            </div>
+                            <div class="description-box">
+                                <?= wpautop($partner_description) ?>
+                            </div>
                         </div>
                     </div>
                 <?php } ?>
             </div>
         </div>
-    </div>
-    <div class="swiper-pagination swiper-pagination-style"></div>
+    <?php } ?>
+
 </div>
 <?
