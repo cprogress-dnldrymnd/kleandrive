6760dd30a00be94540c9c3c41586190bb4c50b11
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 17:04:29 +0800

    css

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -95,10 +95,10 @@ function slider_range($label, $measurement, $id, $min, $max, $start = 0)
                     <div class="tab-content" id="v-pills-tabContent">
                         <div class="tab-pane fade show active" id="v-pills-number-of-buses-converted" role="tabpanel" aria-labelledby="v-pills-number-of-buses-converted-tab">
                             <?= slider_range('Number of buses to be Repowered', 'buses', 'number_of_buses', '0', '100') ?>
-                            <div class="label">
+                            <div class="label mt-5">
                                 <label for="number_of_buses" class="form-label">Are they single or double deck buses</label>
                             </div>
-                            <select name="single_or_double" id="single_or_double" class="mt-5 d-block">
+                            <select name="single_or_double" id="single_or_double" >
                                 <option value="single"> Single </option>
                                 <option value="double"> Double </option>
                             </select>
