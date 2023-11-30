42d21adbc681a7f0f75aef2cc8deddeb2a4e7f54
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 23:09:38 +0800

    Update partners-script.js

--- kleandrive-child/includes/elementor-widgets/partners/partners-script.js
+++ kleandrive-child/includes/elementor-widgets/partners/partners-script.js
@@ -38,13 +38,13 @@ jQuery(document).ready(function () {
         },
         slidesPerView: 1,
         spaceBetween: 30,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
 
         breakpoints: {
