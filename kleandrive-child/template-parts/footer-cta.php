11cf1781d5e8455714f825d85f19fa01c45d6734
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 14:39:00 +0800

    Update footer-cta.php

--- kleandrive-child/template-parts/footer-cta.php
+++ kleandrive-child/template-parts/footer-cta.php
@@ -2,14 +2,15 @@
 
 
 if (is_home()) {
-    $blog_page_id = get_option( 'page_for_posts' );
+    $blog_page_id = get_option('page_for_posts');
 
     $overwrite_footer_cta = carbon_get_post_meta($blog_page_id, 'overwrite_footer_cta');
     $heading = carbon_get_post_meta($blog_page_id, 'heading');
     $button_text = carbon_get_post_meta($blog_page_id, 'button_text');
     $button_url = carbon_get_post_meta($blog_page_id, 'button_url');
     $target = carbon_get_post_meta($blog_page_id, 'target');
-} else {
+}
+else {
     $overwrite_footer_cta = carbon_get_the_post_meta('overwrite_footer_cta');
     $heading = carbon_get_the_post_meta('heading');
     $button_text = carbon_get_the_post_meta('button_text');
@@ -25,26 +26,30 @@ $footer_cta_target = carbon_get_theme_option('footer_cta_target');
 
 if ($overwrite_footer_cta && $heading) {
     $heading_val = $heading;
-} else {
+}
+else {
     $heading_val = $footer_cta_heading;
 }
 
 if ($overwrite_footer_cta && $button_text) {
     $button_text_val = $button_text;
-} else {
+}
+else {
     $button_text_val = $footer_cta_button_text;
 }
 
 if ($overwrite_footer_cta && $button_url) {
     $button_url_val = $button_url;
-} else {
+}
+else {
     $button_url_val = $footer_cta_button_url;
 }
 
 
 if ($overwrite_footer_cta && $target) {
     $target_val = $target;
-} else {
+}
+else {
     $target_val = $target;
 }
 ?>
@@ -60,14 +65,27 @@ if ($overwrite_footer_cta && $target) {
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="sc_item_button sc_button_wrap">
-                    <a target="<?= $target_val ?>" href="<?= $button_url_val ?>" class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
-                        <span class="sc_button_text">
-                            <span class="sc_button_title">
-                                <?= $button_text_val ?>
+                <div class="button-group-box">
+                    <div class="sc_item_button sc_button_wrap">
+                        <a href="/savings-calculator"
+                            class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+                            <span class="sc_button_text">
+                                <span class="sc_button_title">
+                                    SAVINGS CALCULATOR
+                                </span>
                             </span>
-                        </span>
-                    </a>
+                        </a>
+                    </div>
+                    <div class="sc_item_button sc_button_wrap">
+                        <a target="<?= $target_val ?>" href="<?= $button_url_val ?>"
+                            class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+                            <span class="sc_button_text">
+                                <span class="sc_button_title">
+                                    <?= $button_text_val ?>
+                                </span>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
