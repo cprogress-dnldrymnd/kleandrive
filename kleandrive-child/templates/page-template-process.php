5137354ad200f512df00cc7001ed8792c7d7d75f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 17:37:50 +0800

    Update page-template-process.php

--- kleandrive-child/templates/page-template-process.php
+++ kleandrive-child/templates/page-template-process.php
@@ -19,6 +19,7 @@ $subtitle = carbon_get_the_post_meta('subtitle');
         <div class="description-box">
             <?= wpautop($description) ?>
         </div>
+        <?= do_shortcode('[trx_sc_layouts layout="23938"]') ?>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
         </svg>
