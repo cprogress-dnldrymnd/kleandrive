f043b6029b7ce12417c3f7aa3a9217dcc40ba8e2
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-02 02:42:32 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -378,7 +378,7 @@ function slider_range($label, $measurement, $id)
 
             //Compute Total CO2 saved
 
-            total_co2_saved_val = parseFloat((Blended_average_CO2_saving + Incremental_CO2_benefit_vs_New_BEV) * annual_average_distance_travel * average_remaining_life * num_of_buses / 1000000);
+            total_co2_saved_val = parseFloat((Blended_average_CO2_saving + Incremental_CO2_benefit_vs_New_BEV) * num_of_buses * average_remaining_life * annual_average_distance_travel / 1000000);
             
             jQuery('span[result="Total CO2 saved"]').html(total_co2_saved_val.toLocaleString('en-US'));
 
