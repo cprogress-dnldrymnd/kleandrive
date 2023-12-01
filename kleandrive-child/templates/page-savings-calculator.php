b8b48a7aec2faaf0750b74768dc360a45a5f970b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-02 03:56:05 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -322,12 +322,12 @@ function slider_range($label, $measurement, $id)
                     format: {
                         // 'to' the formatted value. Receives a number.
                         to: function(value) {
-                            return parseFloat(value).toFixed(2);
+                            return parseFloat(value);
                         },
                         // 'from' the formatted value.
                         // Receives a string, should return a number.
                         from: function(value) {
-                            return Number(parseFloat(value).toFixed(2));
+                            return Number(parseFloat(value));
                         }
                     }
                 });
