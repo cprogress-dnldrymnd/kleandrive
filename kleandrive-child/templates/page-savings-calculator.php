e663cccea4b673f313864bafef4df2f4046a1872
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-02 03:33:10 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -418,7 +418,8 @@ function slider_range($label, $measurement, $id)
             Grant_BSOG_NSG_savings = (Rate_of_BSOG_NSG_for_repowered_vehicle * annual_average_distance_travel) -  (Current_Rate_of_BSOG * annual_average_distance_travel);
             jQuery('span[result="Grant (BSOG/NSG) savings"]').html(parseInt(Grant_BSOG_NSG_savings).toLocaleString('en-US'));
 
-
+            console.log(Rate_of_BSOG_NSG_for_repowered_vehicle * annual_average_distance_travel)
+            console.log(Current_Rate_of_BSOG * annual_average_distance_travel)
 
             //Compute Capital cost savings over buying new electric buses
             Capital_cost_savings_over_buying_new_electric_buses = 1;
