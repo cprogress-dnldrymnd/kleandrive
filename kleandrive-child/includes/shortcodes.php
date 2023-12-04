255d623832b2ce23de26872ab66a68a511f69fd3
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-05 01:44:01 +0800

    cs

--- kleandrive-child/includes/shortcodes.php
+++ kleandrive-child/includes/shortcodes.php
@@ -1,17 +1,27 @@
 <?php
-function footer_cta_heading()
+function footer_cta_button()
 {
-
+    ob_start();
     if (is_home()) {
         $blog_page_id = get_option('page_for_posts');
+
         $overwrite_footer_cta = carbon_get_post_meta($blog_page_id, 'overwrite_footer_cta');
         $heading = carbon_get_post_meta($blog_page_id, 'heading');
+        $button_text = carbon_get_post_meta($blog_page_id, 'button_text');
+        $button_url = carbon_get_post_meta($blog_page_id, 'button_url');
+        $target = carbon_get_post_meta($blog_page_id, 'target');
     } else {
         $overwrite_footer_cta = carbon_get_the_post_meta('overwrite_footer_cta');
         $heading = carbon_get_the_post_meta('heading');
+        $button_text = carbon_get_the_post_meta('button_text');
+        $button_url = carbon_get_the_post_meta('button_url');
+        $target = carbon_get_the_post_meta('target');
     }
 
     $footer_cta_heading = carbon_get_theme_option('footer_cta_heading');
+    $footer_cta_button_text = carbon_get_theme_option('footer_cta_button_text');
+    $footer_cta_button_url = carbon_get_theme_option('footer_cta_button_url');
+    $footer_cta_target = carbon_get_theme_option('footer_cta_target');
 
 
     if ($overwrite_footer_cta && $heading) {
@@ -20,7 +30,44 @@ function footer_cta_heading()
         $heading_val = $footer_cta_heading;
     }
 
-    return $heading_val;
+    if ($overwrite_footer_cta && $button_text) {
+        $button_text_val = $button_text;
+    } else {
+        $button_text_val = $footer_cta_button_text;
+    }
+
+    if ($overwrite_footer_cta && $button_url) {
+        $button_url_val = $button_url;
+    } else {
+        $button_url_val = $footer_cta_button_url;
+    }
+
+
+    if ($overwrite_footer_cta && $target) {
+        $target_val = $target;
+    } else {
+        $target_val = $target;
+    }
+
+?>
+    <div class="sc_item_button sc_button_wrap">
+        <a href="/savings-calculator" class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+            <span class="sc_button_text">
+                <span class="sc_button_title">
+                    SAVINGS CALCULATOR
+                </span>
+            </span>
+        </a>
+        <a target="<?= $target_val ?>" href="<?= $button_url_val ?>" class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+            <span class="sc_button_text">
+                <span class="sc_button_title">
+                    <?= $button_text_val ?>
+                </span>
+            </span>
+        </a>
+    </div>
+<?php
+    return ob_get_clean();
 }
 
-add_shortcode('footer_cta_heading', 'footer_cta_heading');
+add_shortcode('footer_cta_button', 'footer_cta_button');
--- kleandrive-child/template-parts/footer-cta.php
+++ kleandrive-child/template-parts/footer-cta.php
@@ -9,8 +9,7 @@ if (is_home()) {
     $button_text = carbon_get_post_meta($blog_page_id, 'button_text');
     $button_url = carbon_get_post_meta($blog_page_id, 'button_url');
     $target = carbon_get_post_meta($blog_page_id, 'target');
-}
-else {
+} else {
     $overwrite_footer_cta = carbon_get_the_post_meta('overwrite_footer_cta');
     $heading = carbon_get_the_post_meta('heading');
     $button_text = carbon_get_the_post_meta('button_text');
@@ -26,30 +25,26 @@ $footer_cta_target = carbon_get_theme_option('footer_cta_target');
 
 if ($overwrite_footer_cta && $heading) {
     $heading_val = $heading;
-}
-else {
+} else {
     $heading_val = $footer_cta_heading;
 }
 
 if ($overwrite_footer_cta && $button_text) {
     $button_text_val = $button_text;
-}
-else {
+} else {
     $button_text_val = $footer_cta_button_text;
 }
 
 if ($overwrite_footer_cta && $button_url) {
     $button_url_val = $button_url;
-}
-else {
+} else {
     $button_url_val = $footer_cta_button_url;
 }
 
 
 if ($overwrite_footer_cta && $target) {
     $target_val = $target;
-}
-else {
+} else {
     $target_val = $target;
 }
 ?>
@@ -66,25 +61,24 @@ else {
             </div>
             <div class="col-lg-5">
                 <div class="row justify-content-end">
-                    <div class="sc_item_button sc_button_wrap col-auto">
-                        <a href="/savings-calculator"
-                            class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+                    <div class="sc_item_button sc_button_wrap">
+                        <a href="/savings-calculator" class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
                             <span class="sc_button_text">
                                 <span class="sc_button_title">
                                     SAVINGS CALCULATOR
                                 </span>
                             </span>
                         </a>
-                    </div>
-                    <div class="sc_item_button sc_button_wrap col-auto">
-                        <a target="<?= $target_val ?>" href="<?= $button_url_val ?>"
-                            class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
+                        <a target="<?= $target_val ?>" href="<?= $button_url_val ?>" class="sc_button sc_button_default sc_button_size_normal sc_button_icon_left color_style_dark">
                             <span class="sc_button_text">
                                 <span class="sc_button_title">
                                     <?= $button_text_val ?>
                                 </span>
                             </span>
                         </a>
+                    </div>
+                    <div class="sc_item_button sc_button_wrap col-auto">
+
                     </div>
                 </div>
             </div>
