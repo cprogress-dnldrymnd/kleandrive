a770e64006b125aad429e0e4a882f53d88790665
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-05 22:14:07 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -541,15 +541,10 @@ function slider_range($label, $measurement, $id)
 
 
         //Compute Total NOx damage costs saved
-        console.log(annual_average_distance_travel);
-        console.log(average_remaining_life);
-        console.log(num_of_buses);
-        console.log(NOx_Road_Transport);
-        console.log(Blended_average_NOx_saving_per_1_DD_vehicle);
         if (single_or_double == 'double') {
-            Total_NOx_damage_costs_saved = (annual_average_distance_travel * average_remaining_life * num_of_buses * NOx_Road_Transport * Blended_average_NOx_saving_per_1_DD_vehicle) / 1000000;
+            Total_NOx_damage_costs_saved = (annual_average_distance_travel * average_remaining_life * num_of_buses * NOx_Road_Transport * Blended_average_NOx_saving_per_1_DD_vehicle);
         } else {
-            Total_NOx_damage_costs_saved = annual_average_distance_travel * average_remaining_life * num_of_buses * NOx_Road_Transport * Blended_average_NOx_saving_per_1_SD_vehicle / 1000000;
+            Total_NOx_damage_costs_saved = annual_average_distance_travel * average_remaining_life * num_of_buses * NOx_Road_Transport * Blended_average_NOx_saving_per_1_SD_vehicle;
         }
         jQuery('span[result="Total NOx damage costs saved"]').html('£' + parseInt(Total_NOx_damage_costs_saved).toLocaleString('en-US'));
 
