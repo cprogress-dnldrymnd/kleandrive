eb2896efb139b399b35e6530058b8c0d478c8693
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 17:31:24 +0800

    Update page-savings-calculator.php

--- kleandrive-child/templates/page-savings-calculator.php
+++ kleandrive-child/templates/page-savings-calculator.php
@@ -582,8 +582,8 @@ function slider_range($label, $measurement, $id)
         }
 
         Fuel_savings_final_val = Fuel_savings * average_remaining_life;
-
-        jQuery('span[result="Fuel savings"]').html('£' + Math.round(Fuel_savings + Fuel_savings_final_val).toLocaleString('en-US'));
+        
+        jQuery('span[result="Fuel savings"]').html('£' + Math.round(Fuel_savings).toLocaleString('en-US') + Fuel_savings_final_val);
 
         //Compute Maintenance saving
         Maintenance_saving = (existing_vehicle_service_and_maintenance_cost - 2750) * average_remaining_life * num_of_buses;
